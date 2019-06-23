0b5ea07325dffb2e0553d8a0f9f53e978486bf01
ninja <somalarajuchandrika@gmail.com>
2019-06-23 07:45:31 +0000

    Modified

---
+++ src/main/java/com/hcl/oxbank/okbank/OkbankApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.oxbank.okbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkbankApplication.class, args);
+	}
+
+}
